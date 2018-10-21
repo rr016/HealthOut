7aca3bb8120a39b4b10458b473f834ade1b2f740
Randy Valis <rvalis@towson.edu>
2018-10-21 11:28:34 -0400

    Initial commit

---
+++ app/src/androidTest/java/cosc431/towson/edu/sqlitedemo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package cosc431.towson.edu.sqlitedemo
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("cosc431.towson.edu.sqlitedemo", appContext.packageName)
+    }
+}
