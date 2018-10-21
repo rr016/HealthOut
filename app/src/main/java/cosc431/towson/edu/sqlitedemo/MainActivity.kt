58e4d38e3ff66ee83f8ef5cc451d716fada8eadd
Randy Valis <rvalis@towson.edu>
2018-10-21 13:10:00 -0400

    Implemented simple list of people

--- app/src/main/java/cosc431/towson/edu/sqlitedemo/MainActivity.kt
+++ app/src/main/java/cosc431/towson/edu/sqlitedemo/MainActivity.kt
@@ -2,11 +2,36 @@ package cosc431.towson.edu.sqlitedemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IController {
+    override fun addPerson() {
+        val p = Person("Person ${list.size}", list.size + 15)
+        list = list.plusElement(p)
+        recyclerView.adapter.notifyDataSetChanged()
+    }
+
+    override lateinit var list: List<Person>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        list = getPeople()
+
+        recyclerView.adapter = PersonAdapter(this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        addPersonBtn.setOnClickListener { addPerson() }
     }
+
+    private fun getPeople(): List<Person> {
+        return (0..10).map { Person("Person ${it}", it + 15) }
+    }
+}
+
+interface IController {
+    val list: List<Person>
+    fun addPerson()
 }
---
+++ app/src/main/java/cosc431/towson/edu/sqlitedemo/PersonAdapter.kt
@@ -0,0 +1,28 @@
+package cosc431.towson.edu.sqlitedemo
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.person_item.view.*
+
+class PersonAdapter(val controller: IController) :RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent, false)
+        return PersonViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return controller.list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val person = controller.list.get(position)
+        holder.itemView.personNameTv.text = person.name
+        holder.itemView.personAgeTv.text = person.age.toString()
+    }
+}
+
+class PersonViewHolder(view: View):RecyclerView.ViewHolder(view)
+
+data class Person(var name: String, var age: Int)
