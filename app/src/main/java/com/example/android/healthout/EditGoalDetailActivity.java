bf893a4ced5b0e451d39596c28554b1d825a2c60
rr016 <rafr912@gmail.com>
2019-04-27 14:04:09 -0400

    Useless commit

--- app/src/main/java/com/example/android/healthout/EditGoalDetailActivity.java
+++ app/src/main/java/com/example/android/healthout/EditGoalDetailActivity.java
@@ -52,7 +52,7 @@ public class EditGoalDetailActivity extends AppCompatActivity {
                 sTarget = targetEditText.getText().toString();
                 sPeriod = periodSpinner.getSelectedItem().toString();
 
-                Goal addedGoal = new Goal(sApp, 0, sGoalType, 0, sTarget, sPeriod, 0, null);
+                // Goal addedGoal = new Goal(sApp, 0, sGoalType, 0, sTarget, sPeriod, 0, null);
 
             }
         });
