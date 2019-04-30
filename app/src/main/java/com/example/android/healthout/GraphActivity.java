4edbbdc6686d1d240b9b46dbe60946d97eb0c821
rr016 <rafr912@gmail.com>
2019-04-30 15:37:06 -0400

    Added menu buttons to graph activity

--- app/src/main/java/com/example/android/healthout/GraphActivity.java
+++ app/src/main/java/com/example/android/healthout/GraphActivity.java
@@ -1,9 +1,14 @@
 package com.example.android.healthout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -48,5 +53,76 @@ public class GraphActivity extends AppCompatActivity {
         graph.addSeries(series);
 
         Toast.makeText(getApplicationContext(), "" + app_id + "; " + type_id, Toast.LENGTH_LONG).show();
+    }
+
+    /************************ MENU BAR ************************/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Home - Main Menu
+            case R.id.home:
+                Intent moveToMainMenu = new Intent(GraphActivity.this, MainMenuActivity.class).putExtra("user", user);
+                startActivity(moveToMainMenu);
+                return true;
+
+            // Logout
+            case R.id.one:
+                new AlertDialog.Builder(this)
+                        .setTitle("Logout")
+                        .setMessage("Are you sure you want to logout?")
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent moveToLogin = new Intent(GraphActivity.this, LoginActivity.class);
+                        // Prevent user from returning to this page
+                        moveToLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(moveToLogin);
+                    }
+                }).setIcon(android.R.drawable.ic_dialog_alert).show();
+
+                return true;
+
+            // Edit Account
+            case R.id.two:
+                Intent moveToEditAccount = new Intent(GraphActivity.this, EditAccountActivity.class).putExtra("user", user);
+                startActivity(moveToEditAccount);
+                return true;
+
+            // Delete Account
+            case R.id.three:
+                new AlertDialog.Builder(this)
+                        .setTitle("Confirm Delete")
+                        .setMessage("Are you sure you want to delete this account?")
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "USER_ID = "+user.getUser_id(), Toast.LENGTH_LONG).show();
+                        db.deleteAccountFromUserTable(user.getUser_id());
+
+                        Intent moveToLogin = new Intent(GraphActivity.this, LoginActivity.class);
+                        // Prevent user from returning to this page
+                        moveToLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(moveToLogin);
+                    }
+                }).setIcon(android.R.drawable.ic_dialog_alert).show();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
